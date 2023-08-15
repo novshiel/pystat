22fe4d99a18f350a92358da3c39a59c754b01944
Novshiel Vidal <novshiel@users.noreply.github.com>
2023-08-14 22:49:02 -0700

    Begin adding calc_sum

---
+++ lib/calc_sum.py
@@ -0,0 +1,2 @@
+def calc_sum(numbers):
+    return numbers
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 from lib.add_two import add_two
+from lib.calc_sum import calc_sum
 
 # Test add_two function.
 # Expected output: 5
-print(add_two(2, 3))
+# print(add_two(2, 3))
+
+numbers = [1, 2, 3, 4, 5]
+print(calc_sum(numbers))
+
