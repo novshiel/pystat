e7eac0c703de8c859e3e6151b6538ee3e5993060
Richard Gieg <richgieg@users.noreply.github.com>
2023-08-17 21:25:33 -0700

    Replace double quotes with single quotes

--- test_add_two.py
+++ test_add_two.py
@@ -5,17 +5,17 @@ def test_add_two_positive_numbers():
     test_name = currentframe().f_code.co_name
     result = add_two(2, 3)
     if result == 5:
-        print(test_name + ": passed")
+        print(test_name + ': passed')
     else:
-        print(test_name + ": failed")
+        print(test_name + ': failed')
 
 def test_add_two_negative_numbers():
     test_name = currentframe().f_code.co_name
     result = add_two(-2, -3)
     if result == -5:
-        print(test_name + ": passed")
+        print(test_name + ': passed')
     else:
-        print(test_name + ": failed")
+        print(test_name + ': failed')
 
 def test_add_two():
     test_add_two_positive_numbers()
